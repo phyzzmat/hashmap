9b90bc8609e0702fc8685a5560d748077ec6df28
phyzzmat <35003285+phyzzmat@users.noreply.github.com>
2021-06-25 03:48:47 +0300

    Update hashmap.h

--- hashmap.h
+++ hashmap.h
@@ -1,7 +1,6 @@
-#include<iostream>
-#include<vector>
-#include<stdexcept>
 #include<list>
+#include<stdexcept>
+#include<vector>
 
 template<class KeyType, class ValueType> class Iter;
 template<class KeyType, class ValueType> class ConstIter;
@@ -11,8 +10,9 @@ template<class KeyType, class ValueType> class ConstIter;
 // - the chain list, where chains are implemented with std::list<> and contain indices of data entries in main storage;
 // - the main data storage, implemented with std::vector<std::pair<>>.
 // Resize policy:
-// - if the number of elements exceeds current capacity, capacity is increased to double the original plus one;
-// - if the capacity exceeds four times the number of elements, the size is reduced by half.
+// - if the number of elements exceeds current capacity multiplied by kMaxLoadFactor,
+// capacity is increased to double the original plus one;
+// - if the capacity exceeds kMinScalingFactor times the number of elements, the size is reduced kScalingFactor-fold.
 
 template<class KeyType, class ValueType, class Hash = std::hash<KeyType> >
 class HashMap {
@@ -42,44 +42,34 @@ class HashMap {
 
     // O(1)
     iterator begin() {
-        if (hashmap_.empty()) {
-            return iterator(nullptr);
-        }
-        return iterator(&hashmap_[0]);
+        return iterator(hashmap_.begin());
     }
 
     // O(1)
     iterator end() {
-        if (hashmap_.empty()) {
-            return iterator(nullptr);
-        }
-        return iterator(&hashmap_[0] + stored_elements_);
+        return iterator(hashmap_.end());
     }
 
     // O(1)
     const_iterator begin() const {
-        if (hashmap_.empty()) {
-            return const_iterator(nullptr);
-        }
-        return const_iterator(&hashmap_[0]);
+        return const_iterator(hashmap_.begin());
     }
 
     // O(1)
     const_iterator end() const {
-        if (hashmap_.empty()) {
-            return const_iterator(nullptr);
-        }
-        return const_iterator(&hashmap_[0] + stored_elements_);
+        return const_iterator(hashmap_.end());
     }
 
     // Returns non-const iterator to key if it exists, end() otherwise.
     // Time complexity: amortized O(1).
     iterator find(KeyType key) {
-        if (capacity_ == 0) return end();
-        int candidate_position = hasher_(key) % capacity_;
+        if (capacity_ == 0) {
+            return end();
+        }
+        size_t candidate_position = GetBucketIndex(key);
         for (const auto& element : indices_[candidate_position]) {
             if (hashmap_[element].first == key) {
-                return iterator(&hashmap_[0] + element);
+                return iterator(hashmap_.begin() + element);
             }
         }
         return end();
@@ -88,11 +78,13 @@ class HashMap {
     // Returns const iterator to key if it exists, end() otherwise.
     // Time complexity: amortized O(1), individual query expected O(1), provided the hash function is good enough.
     const_iterator find(KeyType key) const {
-        if (capacity_ == 0) return end();
-        int candidate_position = hasher_(key) % capacity_;
+        if (capacity_ == 0) {
+            return end();
+        }
+        size_t candidate_position = GetBucketIndex(key);
         for (const auto& element : indices_[candidate_position]) {
             if (hashmap_[element].first == key) {
-                return const_iterator(&hashmap_[0] + element);
+                return const_iterator(hashmap_.begin() + element);
             }
         }
         return end();
@@ -101,45 +93,42 @@ class HashMap {
     // Empties the hashmap.
     // Time complexity: O(n), where n is the number of elements contained inside the hashmap.
     void clear() {
-        capacity_ = 0;
+        capacity_ = 1;
         stored_elements_ = 0;
         hashmap_.clear();
         indices_.clear();
-        indices_.resize(1);
+        indices_.resize(capacity_);
     }
 
     // Inserts an element in case there does not already exist one with the same key.
+    // Returns an iterator to the inserted element (or the existing one).
     // Time complexity: amortized O(1), individual query O(n).
-    void insert(std::pair<KeyType, ValueType> x) {
-        if (find(x.first) != end()) {
-            return;
+    iterator insert(std::pair<KeyType, ValueType> element) {
+        auto key_position = find(element.first);
+        if (key_position != end()) {
+            return key_position;
         }
-        if (stored_elements_ >= capacity_) {
-            capacity_ = capacity_ * kScalingFactor + 1;
-            rebuild();
-        }
+        UpscaleIfNecessary();
         ++stored_elements_;
-        int pos = hasher_(x.first) % capacity_;
+        size_t pos = GetBucketIndex(element.first);
         indices_[pos].push_back(stored_elements_ - 1);
-        hashmap_.push_back(x);
+        hashmap_.push_back(element);
+        return iterator(hashmap_.begin() + stored_elements_ - 1);
     }
 
     // Erases an element by key if there exists one, otherwise does nothing.
     // Time complexity: amortized O(1), individual query O(n).
     void erase(KeyType key) {
-        if (find(key) == end()) {
+        iterator key_position = find(key);
+        if (key_position == end()) {
             return;
         }
-        std::pair<KeyType, ValueType> last_element = hashmap_.back();
-        if (last_element.first == key) {
-            erase_last_element();
-        } else {
-            erase_non_last_element(key, last_element);
+        --stored_elements_;
+        if (LastKey() != key) {
+            SwapWithLast(key);
         }
-        if (stored_elements_ * kMinLoadFactor <= capacity_) {
-            capacity_ /= kScalingFactor;
-            rebuild();
-        }
+        EraseLastElement();
+        DownscaleIfNecessary();
     }
 
     // Returns number of elements contained inside the hashmap. O(1)
@@ -156,16 +145,18 @@ class HashMap {
     // Otherwise, inserts a new entry with this key and default value to the hashmap.
     // Time complexity: amortized O(1), individual query O(n).
     ValueType &operator[](KeyType key) {
-        if (find(key) == end()) insert({key, ValueType()});
-        return find(key)->second;
+        return insert({key, ValueType()})->second;
     }
 
     // If there exists an element with given key, returns a constant iterator pointing to it.
     // Otherwise, throws the std::out_of_range() exception.
     // Time complexity: amortized O(1), individual query expected O(1) provided the hash function is good enough.
     const ValueType &at(KeyType key) const {
-        if (find(key) == end()) throw std::out_of_range("");
-        return find(key)->second;
+        auto key_position = find(key);
+        if (key_position == end()) {
+            throw std::out_of_range("");
+        }
+        return key_position->second;
     }
 
     // O(1)
@@ -175,18 +166,19 @@ class HashMap {
 
   private:
 
-    size_t capacity_ = 0, stored_elements_ = 0;
-    // How many times the capacity of the map changes after rebuilding
+    size_t capacity_ = 1, stored_elements_ = 0;
+    // How many times the capacity of the map changes after rebuilding.
     static constexpr size_t kScalingFactor = 2;
-    // The minimum discrepancy between hashmap capacity and number of stored elements for it to be rebuilt
+    // The minimum discrepancies between hashmap capacity and number of stored elements for it to be rebuilt.
     static constexpr size_t kMinLoadFactor = 4;
+    static constexpr size_t kMaxLoadFactor = 2;
 
-    std::vector<std::list<size_t>> indices_;
+    std::vector<std::list<size_t>> indices_ = std::vector<std::list<size_t>>(capacity_);
     std::vector<std::pair<KeyType, ValueType>> hashmap_;
     Hash hasher_;
 
     // Rebuilds and resizes hashmap according to its new capacity.
-    void rebuild() {
+    void Rebuild() {
         std::vector<std::pair<KeyType, ValueType>> old_hashmap;
         old_hashmap = hashmap_;
         stored_elements_ = 0;
@@ -199,10 +191,9 @@ class HashMap {
     }
 
     // Helper function. Erases an element if it is last in data storage.
-    void erase_last_element() {
-        size_t chain_storage_location = hasher_(hashmap_.back().first) % capacity_;
+    void EraseLastElement() {
+        size_t chain_storage_location = GetBucketIndex(LastKey());
         hashmap_.pop_back();
-        --stored_elements_;
         auto it = indices_[chain_storage_location].begin();
         while (*it != stored_elements_) {
             ++it;
@@ -211,61 +202,94 @@ class HashMap {
     }
 
     // Helper function. Erases an element if it is not last in data storage.
-    void erase_non_last_element(const KeyType& key, const std::pair<KeyType, ValueType>& last_element) {
-        int last_element_chain_storage_location = hasher_(last_element.first) % capacity_;
-        int chain_storage_location = hasher_(key) % capacity_;
-        size_t data_storage_location = 0;
-        for (auto i : indices_[chain_storage_location]) {
-            if (hashmap_[i].first == key) data_storage_location = i;
-        }
-        --stored_elements_;
-        auto it = indices_[last_element_chain_storage_location].begin();
-        while (*it != stored_elements_) {
+    void SwapWithLast(const KeyType& key) {
+        auto it1 = FindInChainList(key);
+        auto it2 = FindInChainList(LastKey());
+        std::swap(*it1, *it2);
+        std::swap(hashmap_[*it1], hashmap_[*it2]);
+    }
+
+    // Returns a list iterator pointing to the position in the chain list
+    // where the index of given key is recorded.
+    // Time complexity: amortized O(1), expected O(1), provided the hash function is good enough.
+    std::list<size_t>::iterator FindInChainList(const KeyType &key) {
+        size_t chain_storage_location = GetBucketIndex(key);
+        auto it = indices_[chain_storage_location].begin();
+        while (*it > stored_elements_ || hashmap_[*it].first != key) {
             ++it;
         }
-        *it = data_storage_location;
-        it = indices_[chain_storage_location].begin();
-        while (*it != data_storage_location) {
-            ++it;
+        return it;
+    }
+
+    // Checks if capacity needs to be decreased, and in case it does, performs this.
+    // Time complexity: O(1) amortized, O(n) individual query.
+    void DownscaleIfNecessary() {
+        if (stored_elements_ * kMinLoadFactor <= capacity_) {
+            capacity_ /= kScalingFactor;
+            Rebuild();
         }
-        indices_[chain_storage_location].erase(it);
-        hashmap_[data_storage_location] = last_element;
-        hashmap_.pop_back();
+    }
+
+    // Checks if capacity needs to be increased, and in case it does, performs this.
+    // Time complexity: O(1) amortized, O(n) individual query.
+    void UpscaleIfNecessary() {
+        if (stored_elements_ >= capacity_ * kMaxLoadFactor) {
+            capacity_ *= kScalingFactor;
+            Rebuild();
+        }
+    }
+
+    // Returns index of chain, where given element could be located.
+    // Time complexity: O(1).
+    size_t GetBucketIndex(const KeyType& key) const {
+        return hasher_(key) % capacity_;
+    }
+
+    // Returns constant reference to last key in the data storage.
+    // Time complexity: O(1).
+    const KeyType& LastKey() const {
+        return hashmap_.back().first;
     }
 };
 
 
-// Iterator for HashMap
+// ForwardIterator for HashMap.
 template<class KeyType, class ValueType>
 class Iter {
   public:
     Iter() = default;
 
-    Iter(std::pair<KeyType, ValueType> *ptr) {
-        iter = reinterpret_cast<std::pair<const KeyType, ValueType> *>(ptr);
+    Iter(typename std::vector<std::pair<KeyType, ValueType>>::iterator iter) {
+        iter_ = iter;
     }
 
+    // O(1)
     std::pair<const KeyType, ValueType> operator*() {
-        return *iter;
+        return *reinterpret_cast<std::pair<const KeyType, ValueType>*>(&*iter_);
     }
 
+    // O(1)
     std::pair<const KeyType, ValueType> *operator->() {
-        return iter;
+        return reinterpret_cast<std::pair<const KeyType, ValueType>*>(&*iter_);
     }
 
+    // O(1)
     bool operator==(Iter other) const {
-        return iter == other.iter;
+        return iter_ == other.iter_;
     }
 
+    // O(1)
     bool operator!=(Iter other) const {
-        return iter != other.iter;
+        return iter_ != other.iter_;
     }
 
+    // O(1)
     Iter operator++() {
-        ++iter;
+        ++iter_;
         return *this;
     }
 
+    // O(1)
     Iter operator++(int) {
         Iter copy_iter = *this;
         ++*this;
@@ -273,40 +297,46 @@ class Iter {
     }
 
   private:
-    std::pair<const KeyType, ValueType> *iter;
+    typename std::vector<std::pair<KeyType, ValueType>>::iterator iter_;
 };
 
-// Constant iterator for HashMap
+// Constant ForwardIterator for HashMap.
 template<class KeyType, class ValueType>
 class ConstIter {
   public:
     ConstIter() = default;
 
-    ConstIter(const std::pair<KeyType, ValueType> *ptr) {
-        const_iter = reinterpret_cast<const std::pair<const KeyType, ValueType> *>(ptr);
+    ConstIter(typename std::vector<std::pair<KeyType, ValueType>>::const_iterator const_iter) {
+        const_iter_ = const_iter;
     }
 
+    // O(1)
     const std::pair<const KeyType, ValueType> operator*() const {
-        return *const_iter;
+        return *reinterpret_cast<const std::pair<const KeyType, ValueType>*>(&(*const_iter_));
     }
 
+    // O(1)
     const std::pair<const KeyType, ValueType> *operator->() const {
-        return const_iter;
+        return reinterpret_cast<const std::pair<const KeyType, ValueType>*>(&(*const_iter_));
     }
 
+    // O(1)
     bool operator==(ConstIter other) const {
-        return const_iter == other.const_iter;
+        return const_iter_ == other.const_iter_;
     }
 
+    // O(1)
     bool operator!=(ConstIter other) const {
-        return const_iter != other.const_iter;
+        return const_iter_ != other.const_iter_;
     }
 
+    // O(1)
     ConstIter operator++() {
-        ++const_iter;
+        ++const_iter_;
         return *this;
     }
 
+    // O(1)
     ConstIter operator++(int) {
         ConstIter copy_const_iter = *this;
         ++*this;
@@ -314,5 +344,5 @@ class ConstIter {
     }
 
   private:
-    const std::pair<const KeyType, ValueType> *const_iter;
+    typename std::vector<std::pair<KeyType, ValueType>>::const_iterator const_iter_;
 };
